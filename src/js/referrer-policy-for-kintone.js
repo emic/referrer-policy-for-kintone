2080d69395fd4a3712c0945c9506a0c185b89975
Atsushi Matsuo <famlog@gmail.com>
2020-01-30 15:44:02 +0900

    Fix bug on print preview page

--- src/js/referrer-policy-for-kintone.js
+++ src/js/referrer-policy-for-kintone.js
@@ -1,6 +1,6 @@
 (function() {
   'use strict';
-  kintone.events.on(['app.record.index.show', 'app.record.detail.show', 'app.record.create.show', 'app.record.edit.show', ' 	app.record.print.show', 'app.report.show', 'portal.show'], function(e) {
+  kintone.events.on(['app.record.index.show', 'app.record.detail.show', 'app.record.create.show', 'app.record.edit.show', 'app.record.print.show', 'app.report.show', 'portal.show'], function(e) {
     var element = document.createElement('meta');
     element.setAttribute('name', 'referrer');
     element.setAttribute('content', 'same-origin');
